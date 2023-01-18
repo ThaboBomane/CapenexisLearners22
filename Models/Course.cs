125675ec7c67c6f4ae5f8ede5a69b7b9c65676b0
ThaboBomane <thabobomane2@gmail.com>
2023-01-18 14:40:02 +0200

    Add project files.

---
+++ Models/Course.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapenexisLearners22.Models
+{
+    public class Course
+    {
+        public int Id { get; set; }
+        public string? Lecture { get; set; }
+        public string? CourseName { get; set; }
+        public string? Duration { get; set; }
+        public decimal Price { get; set; }
+    }
+}
---
+++ Models/Facilitator.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CapenexisLearners22.Models
+{
+    public class Facilitator
+    {
+        public int Id { get; set; }
+        public string SurName { get; set; }
+        public string? FirstName { get; set; }
+        public string NationalIdentityNumber { get; set; }
+        public string NumberOfLearners { get; set; }
+    }
+}
